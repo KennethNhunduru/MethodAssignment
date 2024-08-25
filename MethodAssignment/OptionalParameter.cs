ca27846304edc08972ab3df3aec2a0c74ec8c826
KennethNhunduru <133786329+KennethNhunduru@users.noreply.github.com>
2024-08-25 08:06:19 +0100

    Add project files.

---
+++ MethodAssignment/OptionalParameter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class OptionalParameter
+    {
+        /*When a parameter is passed to the method, it is called an ARGUMENT.
+          When calling methods with multiple parameters, the method call must have the same number of arguments 
+          as there are parameters, and the arguments must be in the same order as the parameters.*/
+
+        /*In C# DEFAULT PARAMETER VALUES CAN BE DECLARED WITH THE EQUALS(=) SIGN. 
+          When the method is then called without an argument, the default value is used.
+          A parameter with a default value is often known as an OPTIONAL PARAMETER.*/
+
+        /*It is possible to send arguments with the "key:value" syntax.
+          That way the order of the arguments does not matter.*/
+
+        public int Num1;
+        public int Num2 = 4;
+
+        //CREATING A METHOD THAT TAKES TWO INTEGERS AS PARAMETERS.
+
+        public int MutiplyNumbers(int Num1, int Num2) //SETTING THE DEFAULT VALUE FOR Y = 4, MAKES IT AN OPTIONAL PARAMETER.
+        {
+            int result = Num1 * Num2; //Integer value to be returned by the math operation.
+            Console.WriteLine("The product from multiplying: " + Num1 + " by " + Num2 + " is: " + result);
+            return result;
+        } 
+    }
+}
---
+++ MethodAssignment/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodAssignment
+{
+    class Program
+    {        
+        static void Main(string[] args)
+        {
+            //INSTANTIATING THE OPTIONAL PARAMETER CLASS.
+            OptionalParameter OP = new OptionalParameter();
+
+            //REQUESTING USER INPUT FOR THE FIRST OF TWO NUMBERS.
+
+            Console.WriteLine("Enter the first of your two numbers for the math operation: ");
+            OP.Num1 = Convert.ToInt32(Console.ReadLine());
+
+            //REQUESTING USER INPUT FOR THE OPTIONAL SECOND OF TWO NUMBERS.
+            
+            Request:
+            Console.WriteLine("Do you want to enter the second number for the math operation (Optional)?: \nType in  YES or NO.");
+
+            //capturing the user input in response to request for optional input, and converting it to lowercase.
+            string userChoice = (Console.ReadLine()).ToLower();
+            Console.ReadLine();
+            
+            if (userChoice == "yes")
+            {
+                Console.WriteLine("You have chosen to enter the second number.");
+                Console.WriteLine("Enter your second number: ");
+                OP.Num2 = Convert.ToInt32(Console.ReadLine());
+                Console.ReadLine();                
+            }
+            
+            else if (userChoice == "no")
+            {
+                Console.WriteLine("The default value for the second number will be used for the operation.");
+                Console.ReadLine();                              
+            }
+            
+            else 
+            {
+                Console.WriteLine("Your input data is not in the correct format.");
+                Console.ReadLine();
+                goto Request;
+            }
+            
+            //Calling the "MultiplyNumbers" method in the OptionalParameters class and passing in the one or two numbers entered.
+            OP.MutiplyNumbers(OP.Num1, OP.Num2);
+            Console.ReadLine();                       
+        }
+    }
+}
+
+
+
+
+
+
+
+
